8128eb8a2e82c403378050d70babd3c98af6e2b4
Victor Santos Barbosa <vitinsantosks4@gmail.com>
2025-11-27 21:03:53 -0300

    adicao do toolbar mas falta adicionar exessoes

--- app/src/main/java/com/example/jefiro/barber/home/HomeFragment.java
+++ app/src/main/java/com/example/jefiro/barber/home/HomeFragment.java
@@ -9,14 +9,56 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.jefiro.barber.R;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeFragment extends Fragment {
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    private ShapeableImageView profileImage;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+
+        profileImage = view.findViewById(R.id.profileImage);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() != null) {
+            String uid = mAuth.getCurrentUser().getUid();
+
+            db.collection("Clientes").document(uid).get()
+                    .addOnSuccessListener(documentSnapshot -> {
+                        if (documentSnapshot.exists()) {
+                            String fotoUrl = documentSnapshot.getString("fotoUrl");
+
+                            if (fotoUrl != null && !fotoUrl.isEmpty()) {
+                                Glide.with(this)
+                                        .load(fotoUrl)
+                                        .circleCrop()
+                                        .into(profileImage);
+                            }
+                        }else {
+                            // adicionar exessao
+                            return;
+                        }
+                    });
+        }
     }
 }
