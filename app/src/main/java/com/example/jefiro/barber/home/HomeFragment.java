248ad8ae94096805bb0bedfa68e99aa632c0010c
Victor Santos Barbosa <vitinsantosks4@gmail.com>
2025-11-27 21:13:23 -0300

    inciciando o get da loc do user

--- app/src/main/java/com/example/jefiro/barber/home/HomeFragment.java
+++ app/src/main/java/com/example/jefiro/barber/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.jefiro.barber.home;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,10 +9,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
 import com.example.jefiro.barber.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -19,6 +23,7 @@ public class HomeFragment extends Fragment {
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
     private ShapeableImageView profileImage;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Nullable
     @Override
@@ -32,6 +37,23 @@ public class HomeFragment extends Fragment {
         db = FirebaseFirestore.getInstance();
 
         profileImage = view.findViewById(R.id.profileImage);
+
+//        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return TODO;
+        //}
+//        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(location -> {
+//            if (location != null) {
+//                double latitude = location.getLatitude();
+//                double longitude = location.getLongitude();
+//            }
+//        });
 
         return view;
     }
@@ -54,7 +76,7 @@ public class HomeFragment extends Fragment {
                                         .circleCrop()
                                         .into(profileImage);
                             }
-                        }else {
+                        } else {
                             // adicionar exessao
                             return;
                         }
